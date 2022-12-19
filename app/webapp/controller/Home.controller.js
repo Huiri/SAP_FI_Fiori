b71a35e42ab12f034e00e040a8521cdc21305c17
일환 조 <kin1358@naver.com>
2022-12-19 00:14:02 +0000

    FEAT : 메인 홈 타일 생성

--- app/webapp/controller/Home.controller.js
+++ app/webapp/controller/Home.controller.js
@@ -1,10 +1,30 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+    'sap/m/MessageToast',
+	"sap/ui/model/json/JSONModel",
+	
 ], function(
-	Controller
+	Controller, MessageToast,JSONModel
 ) {
 	"use strict";
-
+	
 	return Controller.extend("project1.controller.Home", {
+		
+		onInit: function(){
+		},
+     
+        onCustomer: function(){
+            this.getOwnerComponent().getRouter().navTo("Customer")
+        },
+		onGLAccount: function(){
+            this.getOwnerComponent().getRouter().navTo("GLAccount")
+        },
+		onCompanyCode: function(){
+            this.getOwnerComponent().getRouter().navTo("CompanyCode")
+        },
+		
+		
+		
+		
 	});
 });
