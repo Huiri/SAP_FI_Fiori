3cacc1856d126dfe087521fed2bc397e39540cc7
Sumin Lee <sm.lee9807@gmail.com>
2022-12-22 06:59:29 +0000

    FIX: 조직 외부 BP 번호 수정

--- app/component/MasterCustomer/controller/CustomerDetail.controller.js
+++ app/component/MasterCustomer/controller/CustomerDetail.controller.js
@@ -166,7 +166,6 @@ sap.ui.define([
 
 				//공통
 				"bp_report_submission": Boolean(v.byId("bpReportSubmission").getSelectedKey()),
-				"bp_external_number": v.byId("bpExternalNumber").getValue(),
 				"bp_changed_date": now,
 				"bp_delivery_rule": v.byId("bpDeliveryRule").getValue(),
 				"bp_vendor": v.byId("bpVendor").getValue(),
